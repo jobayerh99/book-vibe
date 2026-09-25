579a0899fd8e916cc7135b243c928a2580d9ec41
jobayerh99 <jobayerh99@gmail.com>
2026-09-25 23:30:44 +0600

    last time

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -11,14 +11,19 @@ interface IBookDetailsPage {
 }
 
 const getBooks = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch books");
+    if (!res.ok) {
+      throw new Error("Failed to fetch books");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching Book Data", error)
+    return [];
   }
-
-  const data = await res.json();
-  return data;
 };
 
 const BookDetailsPage = async ({ params }: IBookDetailsPage) => {
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -15,6 +15,7 @@ const getBooks = async () => {
     return data;
   } catch (error) {
     console.error("Error fetching Book Data", error)
+    return [];
   }
 };
 
